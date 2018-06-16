d716a9efb6cc4e651f78fd113df22317f2e38118
avenashkumar92 <av.kumar@mum.edu>
2018-06-16 18:44:15 -0500

    Remove package-lock.json, and complete login screen

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {LoginComponent} from './login/login.component'
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { appRoutes } from "./app-routing";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
