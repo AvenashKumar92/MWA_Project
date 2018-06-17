c7486647871566bb1ef181b69a2bacbfcf8d24d4
Alain Mossebo <alainmossebo@gmail.com>
2018-06-16 22:08:08 -0500

    creation of a configured api server with express to save and handle data using mongoose in a mongodb database

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const users = require('../controller/user.controller.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Configuring the database
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/forum'
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(url)
+  .then(() => {
+    console.log("Successfully connected to the database");
+  }).catch(err => {
+    console.log(err);
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+  });
+
+
+/* Save User */
+router.post('/', users.create);
+
 
 module.exports = router;
