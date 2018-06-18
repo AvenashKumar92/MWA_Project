38592a175b575e412ff5742582e55b5dbb7a2edc
Alain Mossebo <alainmossebo@gmail.com>
2018-06-18 08:16:52 -0500

    add method and contents to handle a comment

--- client/src/app/comments/comments.component.ts
+++ client/src/app/comments/comments.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.css']
 })
-export class CommentsComponent implements OnInit {
+export class CommentsComponent {
 
-  constructor() { }
+  comment = {
+    body: '',
+    date_posted: new Date()
+  };
+  public event: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
+  constructor(
+    public dialogRef: MatDialogRef<CommentsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dataService: DataService
+  ) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSubmit(): void {
+    this.event.emit({ data: this.comment });
+    this.dialogRef.close();
   }
 
 }
