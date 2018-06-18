18c83f3ff8b19e75ed6b898a0506751d1d668d80
Alain Mossebo <alainmossebo@gmail.com>
2018-06-18 11:18:11 -0500

    add some css files

--- client/src/app/question/question.component.ts
+++ client/src/app/question/question.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-discussion',
-  templateUrl: './discussion.component.html',
-  styleUrls: ['./discussion.component.css']
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
 
